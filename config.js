39a774033c2c26ffa3ac618919fedba6768091dd
Paolo-sys77 <paolo77@gmail.com>
2026-03-16 22:59:41 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -7,7 +7,5 @@ var SVINCOLATI_DATA_URL = 'svincolati.json';
  *  Sostituisci SUPABASE_ANON_KEY con la tua chiave anon pubblica dal progetto Supabase.
  */
 var SUPABASE_URL = 'https://mmbqekchrorobqwitisv.supabase.co';
-var SUPABASE_ANON_KEY = 'INSERISCI_CHIAVE_ANON_SUPABASE';
-// Nome tabella Supabase con gli svincolati (nel tuo progetto è \"Fmto 2.0\")
-// Verrà URL-encodato nelle chiamate REST, quindi gli spazi non sono un problema.
-var SVINCOLATI_TABLE = 'Fmto 2.0';
+var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tYnFla2Nocm9yb2Jxd2l0aXN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2NDcyMTMsImV4cCI6MjA4OTIyMzIxM30.JacFrD7Y3-RE8uxlHZyx6U0w6z2lY07IrIdZfGfSMTI';
+var SVINCOLATI_TABLE = 'svincolati';
